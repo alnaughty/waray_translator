931209e55561c7f29af2167dbbc705d67dfe5ffc
Zeus <hidarizeus@gmail.com>
2022-05-13 05:36:00 +0800

    dd

--- lib/eng_war_translator.dart
+++ lib/eng_war_translator.dart
@@ -1,1 +1,3 @@
 library eng_war_translator;
+
+export 'package:eng_war_translator/src/translator.dart';
--- lib/src/translator.dart
+++ lib/src/translator.dart
@@ -2,12 +2,13 @@ import 'package:eng_war_translator/services/data_handler.dart';
 import 'package:eng_war_translator/services/string_lookup.dart';
 import 'package:eng_war_translator/services/translate_helper.dart';
 
-class Translator extends TranslateHelper {
+class WarayTranslator extends TranslateHelper {
   final DataHandler _handler = DataHandler();
   final StringLookup _lookup = StringLookup();
   Future<String> translate(String words) async {
     try {
-      return _lookup.fullgram(words, source: await _handler.load());
+      String _ = _lookup.fullgram(words, source: await _handler.load());
+      return _;
       // return ":";
     } catch (e) {
       return words;
